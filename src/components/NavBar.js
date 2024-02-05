60f99213657b2b0a8578c1370baba8e5862b5bdd
Ob55 <brian55mwangi@gmail.com>
2024-02-05 09:22:17 +0300

    done assigment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,30 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return (
+    <nav>
+      {links.map((link, index) => (
+        <a key={index} href={`#${link.toLowerCase()}`}>
+          {link.charAt(0).toUpperCase() + link.slice(1)}
+        </a>
+      ))}
+    </nav>
+  );
 }
 
 export default NavBar;
+
+//import React from "react";
+
+//function NavBar() {
+  //const links = ["home", "about", "projects"];
+ // return <nav>{/* display an <a> tag for each link here */
+   //<> <a href="#home">Home</a>
+    //<a href="#about">About</a>
+    //<a href="#project">projects</a>
+    //</>
+
+//  }</nav>;
+//}
+
+//export default NavBar;
